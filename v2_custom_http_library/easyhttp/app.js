5768490443147243704dff0bafba7a3f899ff6da
Robbie Prokop <robbieprokop@gmail.com>
2022-10-25 21:40:09 +0400

    change app to promise

--- v2_custom_http_library/easyhttp/app.js
+++ v2_custom_http_library/easyhttp/app.js
@@ -1,4 +1,7 @@
 const http = new EasyHTTP();
 
 // get users
-http.get("https://jsonplaceholder.typicode.com/users");
+http
+  .get("https://jsonplaceholder.typicode.com/users")
+  .then((data) => console.log("data :>> ", data))
+  .catch((err) => console.log("err :>> ", err));
--- v2_custom_http_library/easyhttp/easyhttp.js
+++ v2_custom_http_library/easyhttp/easyhttp.js
@@ -2,9 +2,15 @@
 class EasyHTTP {
   //make http get request
   get(url) {
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => console.log("data :>> ", data))
-      .catch((err) => console.log("err :>> ", err));
+    // fetch(url)
+    //   .then((res) => res.json())
+    //   .then((data) => console.log("data :>> ", data))
+    //   .catch((err) => console.log("err :>> ", err));
+    return new Promise((res, rej) => {
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => res(data))
+        .catch((err) => rej(err));
+    });
   }
 }
