8242dfdd8945ee4264d4bf8b705706a44ded0d66
Robbie Prokop <robbieprokop@gmail.com>
2022-10-25 21:06:54 +0400

    create 3 fetch get requests

--- fetch_practice/js_sandbox/app.js
+++ fetch_practice/js_sandbox/app.js
@@ -1,1 +1,28 @@
 // fetch
+const getText = () => {
+  fetch("test.txt")
+    .then((res) => {
+      return res.text();
+    })
+    .then((data) => {
+      document.getElementById("output").innerHTML = data;
+    })
+    .catch((err) => console.log("err :>> ", err));
+};
+
+const getJSON = () => {
+  fetch("posts.json")
+    .then((res) => res.json())
+    .then((data) => (document.getElementById("output").innerHTML = data))
+    .catch((err) => console.log("err >> ", err));
+};
+const getAPI = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => (document.getElementById("output").innerHTML = data))
+    .catch((err) => console.log("err :>> ", err));
+};
+
+document.getElementById("button1").addEventListener("click", getText);
+document.getElementById("button2").addEventListener("click", getJSON);
+document.getElementById("button3").addEventListener("click", getAPI);
