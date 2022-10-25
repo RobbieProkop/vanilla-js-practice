e7c95919fd72d8202f4e98543cefd760e8801428
Robbie Prokop <robbieprokop@gmail.com>
2022-10-25 16:12:54 +0400

    creat eload customers

--- 7_4_project_files/js_sandbox/app.js
+++ 7_4_project_files/js_sandbox/app.js
@@ -26,4 +26,38 @@ const loadCustomer = (e) => {
   xhr.send();
 };
 
+//load multiple customers!
+const loadCustomers = (e) => {
+  e.preventDefault();
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open("GET", "customers.json", true);
+
+  xhr.onload = function () {
+    if (this.status !== 200) {
+      return console.log("uh-oh");
+    }
+    // console.log(this.responseText);
+    const customers = JSON.parse(this.responseText);
+
+    let output = "";
+    customers.forEach((customer) => {
+      output += `
+      <ul>
+        <li>ID: ${customer.id}</li>
+        <li>Name: ${customer.name}</li>
+        <li>Company: ${customer.company}</li>
+        <li>Phone: ${customer.phone}</li>
+      </ul>
+    `;
+    });
+
+    document.getElementById("customers").innerHTML = output;
+  };
+
+  xhr.send();
+};
+
 document.getElementById("button1").addEventListener("click", loadCustomer);
+document.getElementById("button2").addEventListener("click", loadCustomers);
