b2450bb3df8be28f966e9efb07519477f18cb42b
Robbie Prokop <robbieprokop@gmail.com>
2022-10-25 21:36:47 +0400

    create promise get request

--- v2_custom_http_library/easyhttp/easyhttp.js
+++ v2_custom_http_library/easyhttp/easyhttp.js
@@ -1,1 +1,10 @@
 //  Easy HTTP library 2
+class EasyHTTP {
+  //make http get request
+  get(url) {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => console.log("data :>> ", data))
+      .catch((err) => console.log("err :>> ", err));
+  }
+}
