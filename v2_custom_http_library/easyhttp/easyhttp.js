17088b34c350f2c4f9dea087e2ef0f37d4bf2061
Robbie Prokop <robbieprokop@gmail.com>
2022-10-25 21:52:03 +0400

    finish delete request

--- v2_custom_http_library/easyhttp/easyhttp.js
+++ v2_custom_http_library/easyhttp/easyhttp.js
@@ -2,14 +2,47 @@
 class EasyHTTP {
   //make http get request
   get(url) {
-    // fetch(url)
-    //   .then((res) => res.json())
-    //   .then((data) => console.log("data :>> ", data))
-    //   .catch((err) => console.log("err :>> ", err));
     return new Promise((res, rej) => {
       fetch(url)
         .then((res) => res.json())
         .then((data) => res(data))
+        .catch((err) => rej(err));
+    });
+  }
+
+  //make http post request
+  post(url, data) {
+    return new Promise((res, rej) => {
+      fetch(url, {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(data),
+      })
+        .then((res) => res.json())
+        .then((data) => res(data))
+        .catch((err) => rej(err));
+    });
+  }
+
+  // make http put request
+  put(url, data) {
+    return new Promise((res, rej) => {
+      fetch(url, {
+        method: "PUT",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(data),
+      })
+        .then((res) => res.json())
+        .then((data) => res(data))
+        .catch((err) => rej(err));
+    });
+  }
+
+  delete(url) {
+    return new Promise((res, rej) => {
+      fetch(url)
+        .then((res) => res.json())
+        .then((data) => res("deleted"))
         .catch((err) => rej(err));
     });
   }
