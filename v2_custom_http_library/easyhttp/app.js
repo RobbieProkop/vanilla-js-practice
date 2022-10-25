b414d9db4da88909fb59ceab03f478e8fc6ce3e1
Robbie Prokop <robbieprokop@gmail.com>
2022-10-25 21:33:17 +0400

    easyHTTP version 2

---
+++ v2_custom_http_library/easyhttp/app.js
@@ -0,0 +1,46 @@
+// app
+const http = new easyHTTP();
+
+//Get ALl Posts
+http.get("https://jsonplaceholder.typicode.com/posts", (err, res) => {
+  if (err) {
+    return console.log("error :>> ", err);
+  }
+  console.log("res :>> ", res);
+});
+
+// //Get Single Post
+// http.get("https://jsonplaceholder.typicode.com/posts/1", (err, res) => {
+//   if (err) {
+//     return console.log("error :>> ", err);
+//   }
+//   console.log("res :>> ", res);
+// });
+
+const data = {
+  title: "Custom Post",
+  body: "some body text",
+};
+// //Create Post
+// http.post("https://jsonplaceholder.typicode.com/posts", data, (err, res) => {
+//   if (err) {
+//     return console.log("error :>> ", err);
+//   }
+//   console.log("res :>> ", res);
+// });
+
+//update post
+// http.put("https://jsonplaceholder.typicode.com/posts/1", data, (err, res) => {
+//   if (err) {
+//     return console.log("error :>> ", err);
+//   }
+//   console.log("res :>> ", res);
+// });
+
+// delete post
+// http.delete("https://jsonplaceholder.typicode.com/posts/1", (err, res) => {
+//   if (err) {
+//     return console.log("error :>> ", err);
+//   }
+//   console.log("res :>> ", res);
+// });
