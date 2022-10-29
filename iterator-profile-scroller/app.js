6b2dbaefb9a2ff370c5cd6a9e53717425ec94809
Robbie Prokop <robbieprokop@gmail.com>
2022-10-29 16:37:45 +0400

    made mini iterator

--- iterator-profile-scroller/app.js
+++ iterator-profile-scroller/app.js
@@ -34,3 +34,48 @@ const data = [
     image: "https://randomuser.me/api/portraits/women/12.jpg",
   },
 ];
+
+const profiles = profileIterator(data);
+
+//Call first profile
+nextProfile();
+
+//Next Event
+document.getElementById("next").addEventListener("click", nextProfile);
+
+//Next Profile Display
+function nextProfile() {
+  const currentProfile = profiles.next().value;
+  if (currentProfile) {
+    document.getElementById("profileDisplay").innerHTML = `
+      <ul class="list-group">
+        <li class="list-group-item"> Name: ${currentProfile.name}</li>
+        <li class="list-group-item"> Age: ${currentProfile.age}</li>
+        <li class="list-group-item"> Location: ${currentProfile.location}</li>
+      </ul>
+    `;
+
+    document.getElementById("imageDisplay").innerHTML = `
+      <img src="${currentProfile.image}"/>
+    `;
+  } else {
+    //no more profiles
+    window.location.reload();
+  }
+}
+
+// Profile iterator
+function profileIterator(profiles) {
+  let nextIndex = 0;
+
+  return {
+    next: () => {
+      return nextIndex < profiles.length
+        ? {
+            value: profiles[nextIndex++],
+            done: false,
+          }
+        : { done: true };
+    },
+  };
+}
