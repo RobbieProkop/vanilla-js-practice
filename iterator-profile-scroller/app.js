981a08bbf09b7ea8a0a5f80e6f5705534985dfdb
Robbie Prokop <robbieprokop@gmail.com>
2022-10-29 16:22:19 +0400

    create random data

--- iterator-profile-scroller/app.js
+++ iterator-profile-scroller/app.js
@@ -1,1 +1,36 @@
-//app
+//hard coded data. could be API
+
+const data = [
+  {
+    name: "John Doe",
+    age: 28,
+    gender: "male",
+    lookingFor: "female",
+    location: "Calgary, AB",
+    image: "https://randomuser.me/api/portraits/men/42.jpg",
+  },
+  {
+    name: "Yuli Smith",
+    age: 26,
+    gender: "female",
+    lookingFor: "male",
+    location: "Calgary, AB",
+    image: "https://randomuser.me/api/portraits/women/82.jpg",
+  },
+  {
+    name: "Ali Jackson",
+    age: 31,
+    gender: "female",
+    lookingFor: "male",
+    location: "Calgary, AB",
+    image: "https://randomuser.me/api/portraits/women/2.jpg",
+  },
+  {
+    name: "Matilda James",
+    age: 23,
+    gender: "female",
+    lookingFor: "male",
+    location: "Edmonton, AB",
+    image: "https://randomuser.me/api/portraits/women/12.jpg",
+  },
+];
