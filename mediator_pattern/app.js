29c5cbe85b83d883cb21ba69422c46d158e8f29a
Robbie Prokop <robbieprokop@gmail.com>
2022-10-31 16:36:57 +0400

    begin mediator pattern

---
+++ mediator_pattern/app.js
@@ -0,0 +1,14 @@
+class User {
+  constructor(name) {
+    this.name = name;
+    this.chatroom = null;
+  }
+
+  send() {}
+
+  receiver() {}
+}
+
+const Chatroom = function () {
+  let users = {}; // list of users
+};
