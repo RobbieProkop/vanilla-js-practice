92d6fd716efaa4d85f480e9f33757e01ddcdc378
Robbie Prokop <robbieprokop@gmail.com>
2022-10-31 16:47:01 +0400

    create chattroom

--- mediator_pattern/app.js
+++ mediator_pattern/app.js
@@ -4,11 +4,72 @@ class User {
     this.chatroom = null;
   }
 
-  send() {}
+  send(message, to) {
+    this.chatroom.send(message, this, to);
+  }
 
-  receiver() {}
+  receiver(message, from) {
+    console.log(`
+      ${from.name} to ${this.name}: ${message}
+    `);
+  }
 }
 
-const Chatroom = function () {
-  let users = {}; // list of users
-};
+class Chatroom {
+  constructor() {
+    this.users = {};
+  }
+
+  register(user) {
+    users[users.name] = user;
+    user.chatroom = this;
+  }
+
+  send(message, from, to) {
+    if (!to) {
+      //mass message to group
+      for (let key in users) {
+        if (users[key] !== from) {
+          users[key].receive(message, from);
+        }
+      }
+      return;
+    }
+    //single user message
+    return to.receive(message, from);
+  }
+}
+// const Chatroom = function () {
+//   let users = {}; // list of users
+//   return {
+//     register: function (user) {
+//       users[users.name] = user;
+//       user.chatroom = this;
+//     },
+
+//     send: function (message, from, to) {
+//       if (!to) {
+//         //mass message to group
+//         for (let key in users) {
+//           if (users[key] !== from) {
+//             users[key].receive(message, from);
+//           }
+//         }
+//         return;
+//       }
+//       //single user message
+//       return to.receive(message, from);
+//     },
+//   };
+// };
+const brad = new User("Brad");
+const jeff = new User("Jeff");
+const robbie = new User("Robbie");
+
+const chatroom = new Chatroom();
+
+chatroom.register(brad);
+chatroom.register(jeff);
+chatroom.register(robbie);
+
+brad.send("Hello Jeff", jeff);
