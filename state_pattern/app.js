a4f375259233b14fa0b727d8ed8ea30a914f2c2e
Robbie Prokop <robbieprokop@gmail.com>
2022-10-31 18:04:31 +0400

    add jumbotron to homestate

--- state_pattern/app.js
+++ state_pattern/app.js
@@ -1,1 +1,36 @@
 //  State PAttern - Like redux
+// const PageState = function () {
+//   let currentState = new homeState(this);
+
+//   this.init = function () {
+//     this.change(new homeState());
+//   };
+// };
+
+class PageState {
+  constructor() {
+    this.currentState = new homeState(this);
+  }
+
+  init() {
+    this.change(new homeState());
+  }
+
+  change(state) {
+    this.currentState = state;
+  }
+}
+
+// home state
+const homeState = function (page) {
+  document.querySelector("#heading").textContent = null;
+  document.querySelector("#heading").innerHTML = `
+    <div class="jumbotron">
+      <h1 class="display-4">Hello, world!</h1>
+      <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+      <hr class="my-4">
+      <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </div>
+  `;
+};
